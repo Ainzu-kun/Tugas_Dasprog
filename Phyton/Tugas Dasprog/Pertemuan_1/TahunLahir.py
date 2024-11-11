55cbe5237ac1880e0a7732f5d741f987c6ffe863
ainzu-kun <zakyngoding.exe@gmail.com>
2024-11-12 01:28:33 +0700

    update Tahun Lahit

--- Phyton/Tugas Dasprog/Pertemuan_1/TahunLahir.py
+++ Phyton/Tugas Dasprog/Pertemuan_1/TahunLahir.py
@@ -2,6 +2,8 @@
 # NIM       : 2403342
 # Kelas     : RPL 1B
 
+
+print ("\n======Selamat datang======\n")
 Nama = input("Masukkan Nama Anda :  ")
 tahun_lahir = int(input("Masukkan Tahun Lahir Anda : "))
 Umur = 2024 - tahun_lahir
@@ -9,3 +11,4 @@ Umur = 2024 - tahun_lahir
 
 
 print(f"Selamat datang {Nama}, umur kamu adalah {Umur} tahun")
+print("\n============Terimakasih============\n")
